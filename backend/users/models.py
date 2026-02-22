6c23bfa4ca90686bb52e94108554cabb0e728b05
maishameherin13 <maisha.meherin@g.bracu.ac.bd>
2026-02-22 09:58:48 +0600

    "feat: custom user model with email login, drf and jwt config"

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    display_name = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
+    is_private = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
